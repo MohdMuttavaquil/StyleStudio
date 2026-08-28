cc426c8650cfbc4567675db5012694c206b99b1b
MohdMuttavaquil <mmuttavaquil@gmail.com>
2026-08-28 17:45:20 +0530

    add check dtatement

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -2,12 +2,16 @@
 
 import mongoose from "mongoose"
 
-export const dbConnecton = async ()=>{
+export const dbConnecton = async () => {
     try {
-         await mongoose.connect(`${process.env.BD_URL!}`)
-         console.log('database connected')
+        if (!process.env.BD_URL) {
+            throw new Error("BD_URL environment variable is not defined");
+        }
+
+        await mongoose.connect(process.env.BD_URL)
+        console.log('database connected')
     } catch (error) {
         console.log(error)
     }
-   
+
 }
