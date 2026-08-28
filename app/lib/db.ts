69dc7e0f7ccfd5786cf4d5fb67b0b7ac2d9895f7
MohdMuttavaquil <mmuttavaquil@gmail.com>
2026-08-28 17:39:02 +0530

    add database url as string

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -4,7 +4,7 @@ import mongoose from "mongoose"
 
 export const dbConnecton = async ()=>{
     try {
-         await mongoose.connect(process.env.BD_URL!)
+         await mongoose.connect(`${process.env.BD_URL!}`)
          console.log('database connected')
     } catch (error) {
         console.log(error)
